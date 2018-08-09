294ba87edb9777c529212e08fe2f5b42ec1ffcac
alvin-li <lx3li@edu.uwaterloo.ca>
2018-08-09 18:04:42 -0400

    Rename emails to be more descriptive

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,10 +1,10 @@
-class OrderNotifier < ActionMailer::Base
+class OrderMailer < ActionMailer::Base
   default from: 'alvin@example.com'
 
   ##
   # @param [Order] order - The order that was received
   # @return [Mail::Message]
-  def received(order)
+  def order_received_email(order)
     @order = order
     mail to: order.email, subject: "Confirmation for Order #{order.id}"
   end
@@ -12,7 +12,7 @@ class OrderNotifier < ActionMailer::Base
   ##
   # @param [Order] order - The order that was shipped
   # @return [Mail::Message]
-  def shipped(order)
+  def order_shipped_email(order)
     @order = order
     mail to: order.email, subject: "Order Number #{order.id} has been Shipped"
   end
