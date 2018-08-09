55e3b418199fd5b0b8c19e1335a9f77ffeb3140d
alvin-li <lx3li@edu.uwaterloo.ca>
2018-08-09 18:00:47 -0400

    Set up order mailer

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,25 +1,19 @@
 class OrderNotifier < ActionMailer::Base
-  default from: 'Sam Ruby <email@inter.net>'
+  default from: 'alvin@example.com'
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_notifier.received.subject
-  #
+  ##
+  # @param [Order] order - The order that was received
+  # @return [Mail::Message]
   def received(order)
     @order = order
-
-    mail to: order.email, subject: 'The Store Confirmation'
+    mail to: order.email, subject: "Confirmation for Order #{order.id}"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_notifier.shipped.subject
-  #
+  ##
+  # @param [Order] order - The order that was shipped
+  # @return [Mail::Message]
   def shipped(order)
     @order = order
-
-    mail to: order.email, subject: 'The Store Order Shipped'
+    mail to: order.email, subject: "Order Number #{order.id} has been Shipped"
   end
 end
